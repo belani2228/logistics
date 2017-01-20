eaeccec7eb99784d49afcda2f87483445c1b0859
zeta17 <hendrik.zeta@gmail.com>
2017-01-20 18:04:49 +0700

    penyempurnaan dokumen

--- logistics/logistics/doctype/rekap_export/rekap_export.py
+++ logistics/logistics/doctype/rekap_export/rekap_export.py
@@ -4,7 +4,16 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class RekapExport(Document):
-	pass
+	def validate(self):
+		self.set_daftar_container()
+
+	def set_daftar_container(self):
+		against_acc = []
+		for d in self.get('container_list'):
+			if d.container_no not in against_acc:
+				against_acc.append(d.container_no)
+		self.daftar_container = ','.join(against_acc)
--- logistics/logistics/doctype/template_biaya/template_biaya.js
+++ logistics/logistics/doctype/template_biaya/template_biaya.js
@@ -22,7 +22,7 @@ cur_frm.set_query("cost_center", "item_biaya",  function (doc, cdt, cdn) {
 				]
     }
 });
-cur_frm.set_query("expense_account", "item_biaya",  function (doc, cdt, cdn) {
+cur_frm.set_query("expense", "item_biaya",  function (doc, cdt, cdn) {
 	var c_doc= locals[cdt][cdn];
     return {
         filters: {
@@ -31,7 +31,7 @@ cur_frm.set_query("expense_account", "item_biaya",  function (doc, cdt, cdn) {
         }
 	}
 });
-cur_frm.set_query("income_account", "item_biaya",  function (doc, cdt, cdn) {
+cur_frm.set_query("income", "item_biaya",  function (doc, cdt, cdn) {
 	var c_doc= locals[cdt][cdn];
     return {
         filters: {
@@ -50,3 +50,41 @@ cur_frm.cscript.qty = function(doc, cdt, cdn) {
 }
 cur_frm.cscript.buying_rate = cur_frm.cscript.qty;
 cur_frm.cscript.selling_rate = cur_frm.cscript.qty;
+
+//hitung total buying
+var calculate_total_buying = function(frm) {
+    var total_buying = frappe.utils.sum(
+        (frm.doc.item_biaya || []).map(function(i) {
+			return (i.qty * i.buying_rate);
+		})
+    );
+    frm.set_value("total_buying", total_buying);
+}
+frappe.ui.form.on("Template Biaya Item", "buying_amount", function(frm, cdt, cdn) {
+    calculate_total_buying(frm, cdt, cdn);
+})
+frappe.ui.form.on("Template Biaya Item", "qty", function(frm, cdt, cdn) {
+    calculate_total_buying(frm, cdt, cdn);
+})
+frappe.ui.form.on("Template Biaya Item", "buying_rate", function(frm, cdt, cdn) {
+    calculate_total_buying(frm, cdt, cdn);
+})
+
+//hitung total selling
+var calculate_total_selling = function(frm) {
+    var total_selling = frappe.utils.sum(
+        (frm.doc.item_biaya || []).map(function(i) {
+			return (i.qty * i.selling_rate);
+		})
+    );
+    frm.set_value("total_selling", total_selling);
+}
+frappe.ui.form.on("Template Biaya Item", "selling_amount", function(frm, cdt, cdn) {
+    calculate_total_selling(frm, cdt, cdn);
+})
+frappe.ui.form.on("Template Biaya Item", "qty", function(frm, cdt, cdn) {
+    calculate_total_selling(frm, cdt, cdn);
+})
+frappe.ui.form.on("Template Biaya Item", "selling_rate", function(frm, cdt, cdn) {
+    calculate_total_selling(frm, cdt, cdn);
+})
