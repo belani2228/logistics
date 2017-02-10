2d5e8f4be8076f8b90dd83d506cc977433985755
zeta17 <hendrik.zeta@gmail.com>
2017-02-10 17:23:05 +0700

    0.0.15

--- logistics/logistics/doctype/rekap_import/rekap_import_dashboard.py
+++ logistics/logistics/doctype/rekap_import/rekap_import_dashboard.py
@@ -6,7 +6,7 @@ def get_data():
 		'transactions': [
 			{
 				'label': _('Related'),
-				'items': ['Deposite Note']
+				'items': ['Deposite Note', 'Sales Invoice', 'Purchase Invoice']
 			},
 		]
 	}
