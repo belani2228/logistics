94d0b8ffe1cfe9b567c72359b4d6007aabea346c
zeta17 <hendrik.zeta@gmail.com>
2017-07-28 10:11:13 +0700

    1.1.37

--- logistics/__init__.py
+++ logistics/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-__version__ = '1.1.36'
+__version__ = '1.1.37'
 
+#v1.1.37:
+#       - Updat job cost print format
 #v1.1.36:
 #       - Updat rekap export, tdk bisa ganti customer jk sudah ada deposite note
 #v1.1.35:
